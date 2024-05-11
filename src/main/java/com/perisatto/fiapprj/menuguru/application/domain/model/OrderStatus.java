0367aeca4053de66518270e952098e9bedd58d2c
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-05-11 10:09:18 -0300

    Domain model
    Criação dos testes e implementação do Domain Model Order

---
+++ src/main/java/com/perisatto/fiapprj/menuguru/application/domain/model/OrderStatus.java
@@ -0,0 +1,22 @@
+package com.perisatto.fiapprj.menuguru.application.domain.model;
+
+public enum OrderStatus {
+	RECEBIDO(1),
+	EM_PREPARACAO(2),
+	PRONTO(3),
+	FINALIZADO(4);
+	
+	private Integer id;
+	
+	OrderStatus(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
